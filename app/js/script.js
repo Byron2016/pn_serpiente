1806d7e234d1e5f49ed0da67fd97eb3adbe5253a
Byron 2016 <bgva2005@yahoo.com>
2021-07-06 20:25:42 -0500

    P01_Agregar variables al javascript (3.40)

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,18 @@
-console.log("hola mundo");
+// console.log("hola mundo");
+
+// Variables globales
+let context;
+let canvas; //lienzo donde se grafica el juego
+let velocidad = 320; // velocidad del movimiento del gusano (en ms), 78 muy rápido y 500 muy lento
+let puntos = 0; // número de cuadrados rojos comidos por el gusano
+let grosor = 20; // grosor del gusano (en pixeles)
+let nc = 24; // número de cuadrados que forman el ancho y el alto del lienzo
+
+let xv = 0, yv = 0;
+let px = 20, py = 20; // posición (x,y) actual de la cabeza del gusano
+let ax = 20, ay = 20; // posición (x,y) actual del cuadrado rojo
+
+let gusano = []; // array que almacena las coordenadas (x,y) que forman el cuerpo del gusano
+let tam = 0; // tamaño del gusano, en número de cuadrados.
+
+
